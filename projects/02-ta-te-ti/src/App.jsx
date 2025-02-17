e260ac6eaf363fa2002349c6a2494d7fb54b975e
unknown <adrianpartarrieu99@gmail.com>
2025-02-17 16:19:25 -0300

    add more logic game, its funtional

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,36 +1,67 @@
 import { useState } from 'react'
 import './App.css'
 
-import { TURNS } from './utils/consts'
-
-
-const Square = ({ children, updateBoard, index }) =>{
-  return (
-    <div className='square'>{children}</div>
-  )
-}
+import { TURNS, WINNING_COMBOS } from './utils/consts'
+import { WinnerModal } from './components/WinnerModal'
+import { Square } from './components/Square'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNING_COMBOS) {
+      const [a, b, c] = combo
+      if(boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        setWinner(boardToCheck[a])
+        return
+      }
+    }
+    boardToCheck.every((square) => square !== null)
+    && setWinner(false)
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setBoard(newBoard)
+    checkWinner(newBoard)
+    setTurn(newTurn)
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
   return (
     <main className='board'>
-            <section className='game'>
-                {
-                    board.map((_, index) => {
-                    return (
-                      <Square
-                        key= {index}
-                        index= {index}
-                      >
-                        {board[index]}
-                      </Square>
-                    )
-                    })
-                }
-            </section>
-        </main> 
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key= {index}
+                index= {index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X} >{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} ></WinnerModal>
+    </main> 
   )
 }
 
