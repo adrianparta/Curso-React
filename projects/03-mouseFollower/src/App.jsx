5751ca54965e5250af0348ed0b8f014808a88ae8
unknown <adrianpartarrieu99@gmail.com>
2025-02-20 16:16:27 -0300

    added mouseFollower project

--- projects/03-mouseFollower/src/App.jsx
+++ projects/03-mouseFollower/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enable, setEnable] = useState(true)
+  const [position, setPosition] = useState({ x: 0, y: 0})
 
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    
+    if(enable){
+      addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
+
+  const handleClick = () => {
+    const newEnable = !enable
+    setEnable(newEnable)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}></div>
+      <button onClick={handleClick}>{enable ? 'Desactivar' : 'Activar'} seguimiento</button>
+    </main>
+
   )
 }
 
