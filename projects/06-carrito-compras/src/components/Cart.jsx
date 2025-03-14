5ea100e038da742e80280a31ccee16e87fbbd0b4
Adrian Partarrieu <adrianpartarrieu99@gmail.com>
2025-03-14 10:39:49 -0300

    feat: add Cart component with checkbox for cart visibility

---
+++ projects/06-carrito-compras/src/components/Cart.jsx
@@ -0,0 +1,31 @@
+import { useId } from "react"
+
+export const Cart = () => {
+    const cartCheckboxId = useId()
+
+    return (
+        <>
+            <label className="cart-button" htmlFor={cartCheckboxId}></label>
+            <input type="checkbox" id={cartCheckboxId} />
+
+            <aside className="cart">
+                <ul>
+                    <li>
+                        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Iphone" />
+                        <div>
+                            <strong>Iphone</strong>
+                            <strong>$1500</strong>
+                        </div>
+
+                        <footer>
+                            <small>Qty: 1</small>
+                            <button>+</button>
+                        </footer>
+                    </li>
+                </ul>
+
+                <button>Clear cart</button>
+            </aside>    
+        </>
+    )
+}
